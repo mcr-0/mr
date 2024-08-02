32db64c844938e7dd2fb9d261e89a3ffb8afe970
Michal <m@michals-mbp.home>
2024-08-02 06:07:59 +0200

    Google tag added

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,15 +5,13 @@ import Image from "next/image";
 import * as React from "react";
 import Link from "next/link";
 import Script from "next/script";
-
+import { NextUIProvider } from "@nextui-org/system";
 const inter = Inter({ subsets: ["latin"] });
-
 export const metadata: Metadata = {
   title: "Mazed.gg - Get Access To Deals And Promos",
   description:
     "Play Games And Get Access to Over 30+ Deals And Promos That Can Make You Real Money",
 };
-
 export default function RootLayout({
   children,
 }: Readonly<{
@@ -21,11 +19,30 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <head>
+        <Script
+          strategy="afterInteractive"
+          src="https://www.googletagmanager.com/gtag/js?id=AW-11255487453"
+        />
+        <Script
+          id="gtag-init"
+          strategy="afterInteractive"
+          dangerouslySetInnerHTML={{
+            __html: `
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+              gtag('config', 'AW-11255487453');
+            `,
+          }}
+        />
+      </head>
       <body
         className={`${inter.className} relative -z-20 h-full w-full bg-black bg-gradient-to-r from-neutral-900 via-blue-800 to-neutral-900`}
       >
-        <div className="fixed bottom-0 left-0 right-0 -z-10 h-full w-full object-cover object-center opacity-100">
-          {/* <Image
+        <NextUIProvider>
+          <div className="fixed bottom-0 left-0 right-0 -z-10 h-full w-full object-cover object-center opacity-100">
+            {/* <Image
             src="/blue-bg.png"
             alt="Background Image"
             className="h-full w-full object-cover opacity-100"
@@ -33,78 +50,79 @@ export default function RootLayout({
             height={877}
             priority
           ></Image> */}
-        </div>
-        <div className="fixed bottom-0 left-0 right-0 z-10 h-20 w-full bg-gradient-to-t from-black to-black/0"></div>
-        <header className="mx-auto bg-gradient-to-b from-black to-black/0 py-1 text-center sm:px-6 lg:px-8">
-          <Link href="/" className="mx-auto block w-16 shadow-xl">
-            <Image
-              src="/logo.jpg"
-              height={400}
-              width={600}
-              alt="logo"
-              className="h-full w-full rounded-md object-cover"
-            ></Image>
-          </Link>
-          <div className="flex items-center justify-center space-x-2 text-center text-xl text-white">
-            <div className="">
-              {/* <span className="animate-pulse">Status: </span> */}
-              <span className="animate-pulse text-orange-400">
-                Limited spots available
-              </span>
+          </div>
+          <div className="fixed bottom-0 left-0 right-0 z-10 h-20 w-full bg-gradient-to-t from-black to-black/0"></div>
+          <header className="mx-auto bg-gradient-to-b from-black to-black/0 py-1 text-center sm:px-6 lg:px-8">
+            <Link href="/" className="mx-auto block w-16 shadow-xl">
+              <Image
+                src="/logo.jpg"
+                height={400}
+                width={600}
+                alt="logo"
+                className="h-full w-full rounded-md object-cover"
+              ></Image>
+            </Link>
+            <div className="flex items-center justify-center space-x-2 text-center text-xl text-white">
+              <div className="">
+                {/* <span className="animate-pulse">Status: </span> */}
+                <span className="animate-pulse text-orange-400">
+                  Limited spots available
+                </span>
+              </div>
+              <svg
+                width="24"
+                height="24"
+                viewBox="0 0 20 20"
+                fill="none"
+                xmlns="http://www.w3.org/2000/svg"
+              >
+                <circle cx="10" cy="10" r="10" fill="#8FFF00" />
+                <path
+                  d="M5 10L8.5 13.5L14.5 6.5"
+                  stroke="#1E1E1E"
+                  stroke-width="1.5"
+                />
+              </svg>
             </div>
-            <svg
-              width="24"
-              height="24"
-              viewBox="0 0 20 20"
-              fill="none"
-              xmlns="http://www.w3.org/2000/svg"
+
+            <h1 className="bg-gradient-to-r from-blue-200 to-blue-700 bg-clip-text text-5xl font-black tracking-tighter text-transparent">
+              Get Access ^{" "}
+            </h1>
+            <h1 className="bg-gradient-to-r from-blue-500 to-blue-600 bg-clip-text text-xl font-bold leading-8 tracking-tighter text-transparent">
+              <span className="">To Deals And Promos Now!</span>
+            </h1>
+
+            <p className="mt-0 text-xs font-semibold uppercase text-neutral-200">
+              ^Upon completion two steps
+            </p>
+          </header>
+          <div className="relative p-4">
+            <div className="isolate mx-auto w-full max-w-sm items-center justify-center rounded-3xl bg-white/70 p-2 shadow-lg ring-1 ring-black/5 backdrop-blur-md">
+              {children}
+            </div>
+          </div>
+          <p className="mx-auto max-w-sm px-4 pb-20 text-center text-xs text-neutral-400">
+            We are not affiliated with any of the games or companies shown on
+            this website. Use of any logos or trademarks are for reference
+            purposes only. By using the website, you agree to our{" "}
+            <Link
+              href="/terms"
+              target="_blank"
+              className="text-neutral-300 underline"
             >
-              <circle cx="10" cy="10" r="10" fill="#8FFF00" />
-              <path
-                d="M5 10L8.5 13.5L14.5 6.5"
-                stroke="#1E1E1E"
-                stroke-width="1.5"
-              />
-            </svg>
-          </div>
-
-          <h1 className="bg-gradient-to-r from-blue-200 to-blue-700 bg-clip-text text-5xl font-black tracking-tighter text-transparent">
-            Get Access ^{" "}
-          </h1>
-          <h1 className="bg-gradient-to-r from-blue-500 to-blue-600 bg-clip-text text-xl font-bold leading-8 tracking-tighter text-transparent">
-            <span className="">To Deals And Promos Now!</span>
-          </h1>
-
-          <p className="mt-0 text-xs font-semibold uppercase text-neutral-200">
-            ^Upon completion two steps
+              Terms
+            </Link>{" "}
+            and{" "}
+            <Link
+              href="/privacy"
+              target="_blank"
+              className="text-neutral-300 underline"
+            >
+              Privacy Policy
+            </Link>
+            .
           </p>
-        </header>
-        <div className="relative p-4">
-          <div className="isolate mx-auto w-full max-w-sm items-center justify-center rounded-3xl bg-white/70 p-2 shadow-lg ring-1 ring-black/5 backdrop-blur-md">
-            {children}
-          </div>
-        </div>
-        <p className="mx-auto max-w-sm px-4 pb-20 text-center text-xs text-neutral-400">
-          We are not affiliated with any of the games or companies shown on this
-          website. Use of any logos or trademarks are for reference purposes
-          only. By using the website, you agree to our{" "}
-          <Link
-            href="/terms"
-            target="_blank"
-            className="text-neutral-300 underline"
-          >
-            Terms
-          </Link>{" "}
-          and{" "}
-          <Link
-            href="/privacy"
-            target="_blank"
-            className="text-neutral-300 underline"
-          >
-            Privacy Policy
-          </Link>
-          .
-        </p>
+        </NextUIProvider>
       </body>
       <Script src="https://scripts.simpleanalyticscdn.com/latest.js" />
     </html>
